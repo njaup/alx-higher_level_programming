47e587314aa5b2ff0a17c22c0373c31a4a2ac36d
njaup <njaup974@gmail.com>
2023-08-14 12:23:59 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []
-    for num in my_list:
-        result.append(num % 2 == 0)
-    return result
+    total = []
+    for count in my_list:
+        total.append(count % 2 == 0)
+    return total
