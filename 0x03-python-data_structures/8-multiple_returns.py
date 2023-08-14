30a40a0c2a6f8d6cb36d3a51b63c77798879a6a6
njaup <njaup974@gmail.com>
2023-08-14 11:43:43 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return (None, None)
+        return(None, None)
 
-    return (len(sentence), sentence[0])
+    return(len(sentence), sentence[0])
