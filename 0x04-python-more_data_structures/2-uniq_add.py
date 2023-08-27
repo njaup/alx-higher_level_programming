9aea33043f8d0c025898f45f0ffae8577c9d8993
njaup <njaup974@gmail.com>
2023-08-27 10:47:32 -0700

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    total_sum = 0
+    
+    for num in my_list:
+        if num not in unique_integers:
+            total_sum += num
+            unique_integers.add(num)
+    
+    return total_sum
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+result = uniq_add(numbers)
