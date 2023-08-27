1731dea291dfdeb9a8f6463d35e537793dc7cad2
njaup <njaup974@gmail.com>
2023-08-27 12:30:10 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,9 +2,8 @@
 def common_elements(set_1, set_2):
     common_chars = set_1.intersection(set_2)
     if len(common_chars) == 1 and 'C' in common_chars:
-        return list(common_chars)
-    else:
-        return []
+        return liist(common_chars)
+    return []
 
 str1 = "AABBC"
 str2 = "BCDEF"
