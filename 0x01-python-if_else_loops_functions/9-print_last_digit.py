6b2d5e941486b9eb98daa7ce3aab65576df8cdb0
njaup <njaup974@gmail.com>
2024-01-18 11:18:22 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if not isinstance(number, int):
-        print("Please provide an integer.")
-        return (None)
     if number < 0:
         last_digit = number % -(10)
         print(-(last_digit), end='')
