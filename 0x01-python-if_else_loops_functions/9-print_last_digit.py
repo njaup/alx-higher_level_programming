7818f92d2622976dfb3d8c677a61daeb0c140521
njaup <njaup974@gmail.com>
2024-01-18 11:06:49 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if not isinstance(number, int):
-        print("Please provide an integer.")
-        return None
+    try:
+        if not isinstance(number, int):
+            raise ValueError("Please provide an integer.")
 
-    if number < 0:
-        last_digit = number % -(10)
-        print(-(last_digit), end='')
-    else:
-        last_digit = number % 10
-        print(last_digit, end='')
+        if number < 0:
+            last_digit = number % -(10)
+            print(-(last_digit), end='')
+        else:
+            last_digit = number % 10
+            print(last_digit, end='')
 
-    return abs(last_digit)
+        return abs(last_digit)
+    except ValueError as e:
+        print(e)
