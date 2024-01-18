408c6139dd0bcce29d627d3997eb40755522cd5e
njaup <njaup974@gmail.com>
2024-01-18 09:05:01 -0800

    7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def islower(c)
+    return ord('a') <= ord(c) <= ord('z')
+char = 'a'
+result = islower(char)
+print(result)
+
+char = 'B'
+result = islower(char)
+print(result)
