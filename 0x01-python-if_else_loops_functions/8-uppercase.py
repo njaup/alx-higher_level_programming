a8e9151460bef693891e87729e87edbedc120f2e
njaup <njaup974@gmail.com>
2024-01-18 09:46:31 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,5 @@
 def uppercase(input_str):
     for m in input_str:
         if ord(m) >='a' and ord(m) <= 'z':
-            m = chr(ord(m) - ord('a') + ord('A'))
-            print(m, end='')
-        else:
-            print(m, end='')
+            m = chr(ord(m) - 32)
+            print('{}'.format(m), end='')
