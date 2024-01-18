94a7c991803744991ce743ffdda06dd298996414
njaup <njaup974@gmail.com>
2024-01-18 11:21:01 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def print_last_digit(number):
-    if number < 0:
-        last_digit = number % -(10)
-        print(-(last_digit), end='')
+def print_last_digit(num):
+    if num < 0:
+        lastdig = num % -(10)
+        print(-(lastdig), end='')
     else:
-        last_digit = number % 10
-        print(last_digit, end='')
-    return abs(last_digit)
+        lastdig = num % 10
+        print(lastdig, end='')
+    return abs(lastdig)
