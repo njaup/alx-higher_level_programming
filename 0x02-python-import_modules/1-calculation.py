b9a23a8b2ea2235ee19c74755b6075732237ef58
njaup <njaup974@gmail.com>
2024-01-20 10:51:26 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiply, divide
+
+    a = 10
+    b = 5
+    result_add = add(a, b)
+    result_subtract = subtract(a, b)
+    result_multiply = multiply(a, b)
+    result_divide = divide(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result_add))
+    print("{:d} - {:d} = {:d}".format(a, b, result_subtract))
+    print("{:d} * {:d} = {:d}".format(a, b, result_multiply))
+    print("{:d} / {:d} = {:d}".format(a, b, result_divide))
