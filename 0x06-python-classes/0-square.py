d3e1762fb6cfa6578055af9ec3740986608b5470
njaup <njaup974@gmail.com>
2024-02-06 07:10:55 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-'''A simple example demonstrating the definition of a Square class.'''
+"""A simple example demonstrating the definition of a Square class."""
 
 class Square:
-    '''A class representing a square shape.'''
+    """A class representing a square shape."""
     pass
