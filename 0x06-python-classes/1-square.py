7a9047905553d3dcd3c74ce85594482dde10b66c
njaup <njaup974@gmail.com>
2024-02-06 07:21:52 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 
 class Square:
-    """A class to represent a square with a given size."""
+    """
+    A class to represent a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
 
     def __init__(self, size):
-        """Initialize the Square with the given size."""
+        """
+        Initializes a square with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
 
     def __str__(self):
-        """Return a string representation of the square."""
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: A string describing the square.
+        """
         return f"Square with size: {self.__size}"
