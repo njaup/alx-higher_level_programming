9ac0214b36f05c3145dcf9f7c62a2cb9ca734540
njaup <njaup974@gmail.com>
2024-02-18 05:43:44 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    """Return the integer addition of a and b.
 
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-
-    return int(a + b)
-
-if __name__ == "__main__":
-    result = add_integer(5, 3.5)
-    print(result)
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float."""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or a float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or a float")
+    return int(a) + int(b)
