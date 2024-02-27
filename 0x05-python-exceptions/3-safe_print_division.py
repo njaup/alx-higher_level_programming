252b9b22c13bcd035d0870546e74132c59bf377d
njaup <njaup974@gmail.com>
2024-02-27 13:42:00 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -11,4 +11,4 @@ def safe_print_division(a, b):
         return None
     finally:
         print("Inside result: {}".format(value) if 'result' in locals() else "Inside result: None")
-        return (value)
+        return value
