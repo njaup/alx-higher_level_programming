96d7bda559d49bc6ec005f4b51f5c4e6938235af
njaup <njaup974@gmail.com>
2024-02-27 13:40:05 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        value = a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+        return None
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+    finally:
+        print("Inside result: {}".format(value) if 'result' in locals() else "Inside result: None")
+        return (value)
