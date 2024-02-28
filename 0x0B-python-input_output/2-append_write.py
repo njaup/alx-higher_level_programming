0ff16184b2128935d2a3fc7543157f5e54968fd1
njaup <njaup974@gmail.com>
2024-02-27 16:42:41 -0800

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """Append the given text to the end of the specified file.
+
+    Args:
+        filename (str): The path to the file to which text will be appended.
+        text (str): The text to append to the file.
+
+    Returns:
+        int:
+
+    Raises:
+        Exception: If an error occurs during the file operation."""
+    try:
+        with open(filename, 'a', encoding='utf-8') as file:
+            file.write(text)
+            return len(text)
+    except Exception as e:
+        print("An error occurred:", e)
+        return 0
