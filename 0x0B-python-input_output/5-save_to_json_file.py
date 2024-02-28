7ef6fce88be268a51e361995b4f04ef467556bce
njaup <njaup974@gmail.com>
2024-02-27 18:28:23 -0800

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a JSON file-writing function."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Serialize the given object to a JSON representation and save it to a file.
+
+    Args:
+    my_obj: The object to be serialized.
+    filename: The name of the file to save the JSON representation to.
+
+    Returns:
+    None"""
+    try:
+        with open(filename, 'w') as file:
+            json.dump(my_obj, file)
+        print("Object saved to", filename)
+    except:
+        print("Error saving object to", filename)
