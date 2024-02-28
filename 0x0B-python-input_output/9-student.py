a09f285a204728ed2dd0f7912d9e0200f03732cd
njaup <njaup974@gmail.com>
2024-02-28 08:32:58 -0800

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object with the provided first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary containing the first name, last name, and age of the student.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
