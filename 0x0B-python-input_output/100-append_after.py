5b289103e5f31fc475b12470d62fca204ca8cdf5
njaup <njaup974@gmail.com>
2024-03-05 02:38:02 -0800

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file.
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert."""
+    txt = ""
+    with open(filename) as r:
+        for line in r:
+            txt += line
+            if search_string in line:
+                txt += new_string
+    with open(filename, "w") as w:
+        w.write(txt)
