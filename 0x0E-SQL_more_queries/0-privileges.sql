c4e8c550b526d3e52edd907274340d8b97335b53
njaup <njaup974@gmail.com>
2024-03-27 13:34:48 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-show_user_grants("user_0d_1")@"localhost";
-show_user_grants("user_0d_2")@"localhost";
+show_user_grants_for "user_0d_1"@"localhost";
+show_user_grants_for "user_0d_2"@"localhost";
