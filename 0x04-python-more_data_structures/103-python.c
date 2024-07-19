38fb843f3c34a3580567568b71b3d856f4a067a7
njaup <njaup974@gmail.com>
2024-07-19 11:17:53 -0700

    103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
+void print_python_bytes(PyObject *p)
+{
+	long int size;
+	int i;
+	char *trying_str = NULL;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	PyBytes_AsStringAndSize(p, &trying_str, &size);
+
+	printf("  size: %li\n", size);
+	printf("  trying string: %s\n", trying_str);
